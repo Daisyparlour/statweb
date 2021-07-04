4a6a08276ac1879a2faa3b0c8737123499571da4
Daisyparlour <86738832+Daisyparlour@users.noreply.github.com>
2021-07-04 22:21:53 +0530

    mail test

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'daisys.parlour@gnail.com';
+  $receiving_email_address = 'care@daisysalons.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['mobile'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
+    'host' => 'smtpout.secureserver.net',
+    'username' => 'care@daisysalons.com',
+    'password' => 'Miku2468',
+    'port' => '465'
   );
-  */
+ 
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
